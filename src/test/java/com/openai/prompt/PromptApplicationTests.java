6063bb11bfd1b38a343b2fee36f75dcab6115d34
rhsin <ryanhsin@ymail.com>
2023-06-02 18:03:54 -0700

    initialized Spring Boot project

---
+++ src/test/java/com/openai/prompt/PromptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openai.prompt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
