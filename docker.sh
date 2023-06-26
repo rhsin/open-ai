0661ed0ddb1a08bcf967d31053da867f06213bac
rhsin <ryanhsin@ymail.com>
2023-06-26 00:16:36 -0700

    added AWS-ECR push to docker script

--- docker.sh
+++ docker.sh
@@ -1,4 +1,5 @@
-mvn package
 docker build -t ryan3369/open-ai-promt .
-docker push ryan3369/open-ai-promt:latest
+docker tag ryan3369/open-ai-promt:latest 587609106891.dkr.ecr.us-east-2.amazonaws.com/openai-prompt:latest
+docker push 587609106891.dkr.ecr.us-east-2.amazonaws.com/openai-prompt:latest
+# docker push ryan3369/open-ai-promt:latest
 # docker run -p 8080:8080 ryan3369/open-ai-promt:latest
