fa81585834d076086286a2cd57654136e44d0982
rhsin <ryanhsin@ymail.com>
2023-06-25 19:45:18 -0700

    updated docker push script

--- docker.sh
+++ docker.sh
@@ -1,3 +1,4 @@
 mvn package
-docker build -t openai/prompt .
-docker run -p 8080:8080 openai/prompt
+docker build -t ryan3369/open-ai-promt .
+docker push ryan3369/open-ai-promt:latest
+# docker run -p 8080:8080 ryan3369/open-ai-promt:latest
