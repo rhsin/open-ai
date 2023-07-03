375efba14c813a82c7953e9ef6a24dff560f2ee6
rhsin <ryanhsin@ymail.com>
2023-07-03 02:53:32 -0700

    testing AWS-ECS deploy

--- docker.sh
+++ docker.sh
@@ -1,5 +1,5 @@
 docker build -t ryan3369/open-ai-promt .
-docker tag ryan3369/open-ai-promt:latest 587609106891.dkr.ecr.us-east-2.amazonaws.com/openai-prompt:latest
-docker push 587609106891.dkr.ecr.us-east-2.amazonaws.com/openai-prompt:latest
+docker tag ryan3369/open-ai-promt:latest public.ecr.aws/r3p0h4n8/openai:latest
+docker push public.ecr.aws/r3p0h4n8/openai:latest
 # docker push ryan3369/open-ai-promt:latest
 # docker run -p 8080:8080 ryan3369/open-ai-promt:latest
