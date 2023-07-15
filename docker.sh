39d03b5697f8ab776f8840adc9800c35221a569a
rhsin <ryanhsin@ymail.com>
2023-07-15 16:42:30 -0700

    removed docker script

--- docker.sh
+++
@@ -1,5 +0,0 @@
-docker build -t ryan3369/open-ai-promt .
-docker tag ryan3369/open-ai-promt:latest public.ecr.aws/r3p0h4n8/openai:latest
-docker push public.ecr.aws/r3p0h4n8/openai:latest
-# docker push ryan3369/open-ai-promt:latest
-# docker run -p 8080:8080 ryan3369/open-ai-promt:latest
